351629ee3b3917859745486a503f0e1658ed5b58
Vipul Behl <vipulbehl94@gmail.com>
2016-08-14 13:03:40 +0530

    Adding More Sample (Issue #15)
    Adding more sample code to make the use of library easier for new users.

--- sample/src/main/java/com/toptoche/sample/MainActivity.java
+++ sample/src/main/java/com/toptoche/sample/MainActivity.java
@@ -2,6 +2,11 @@ package com.toptoche.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
+		//Adding more sample change #15
+		//Change Start
+		SearchableSpinner searchableSpinner = (SearchableSpinner) findViewById(R.id.spinner);
+        searchableSpinner.setTitle("Choose");
+        searchableSpinner.setPositiveButton("OK");
+        searchableSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Manage all the items clicks here
+				Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+		//change end
     }
 }
