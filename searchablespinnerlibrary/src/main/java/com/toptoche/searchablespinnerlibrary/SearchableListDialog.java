ac57db04a0d88ebb23e8872a37d43a7a883f9ab5
Mohammad Mirzakhani <m.mirzakhani@rightel.ir>
2018-12-11 09:37:55 +0330

    fix gradle:  add support.v4

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
@@ -2,11 +2,11 @@ package com.toptoche.searchablespinnerlibrary;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
