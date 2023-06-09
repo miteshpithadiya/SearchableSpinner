3b3a3fb2b3aecb5f565fb1f914b4b673c8335cbd
Juan Arias <57142765+JuanArias-code@users.noreply.github.com>
2023-06-08 21:09:48 -0500

    Update SearchableListDialog.java
    Spanish Translate

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
@@ -96,7 +96,7 @@ public class SearchableListDialog extends DialogFragment implements
         String strPositiveButton = _strPositiveButtonText == null ? "CLOSE" : _strPositiveButtonText;
         alertDialog.setPositiveButton(strPositiveButton, _onClickListener);
 
-        String strTitle = _strTitle == null ? "Select Item" : _strTitle;
+        String strTitle = _strTitle == null ? "Seleccionar Marca : _strTitle;
         alertDialog.setTitle(strTitle);
 
         final AlertDialog dialog = alertDialog.create();
